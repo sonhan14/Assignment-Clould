d19099f5b231308953e8f68c9440291b78796bf7
sonhan14 <sonhan14@gmail.com>
2021-12-15 12:25:19 +0700

    edit

--- app.js
+++ app.js
@@ -12,29 +12,16 @@ app.set('view engine', 'hbs')
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended : true}))
 
-app.get('/', (req,res)=>{
-    res.render('index')
-})
-
 app.get('/product',(req,res)=>{
     res.render('product')
 })
 
-app.get('/delete',async(req,res)=>{
-    const id = req.query.id
-    console.log("id can xoa: " + id)
-    const dbo = await getDatabase()
-    dbo.collection("Products").deleteOne({_id:ObjectId(id)})
-    res.redirect('/')
-})
-
-app.get('/view',async (req,res)=>{
-    //1. lay du lieu tu mongo
+app.get('/',async(req,res)=>{
     const dbo = await getDatabase()
     const result = await dbo.collection("Products").find({}).toArray()
-    //2. hien thi du lieu qua hbs
     res.render('index',{products:result})
 })
+
 
 app.post('/product',async (req,res)=>{
     const nameInput = req.body.txtName
@@ -52,6 +39,23 @@ app.post('/product',async (req,res)=>{
     const result = await dbo.collection("Products").insertOne(newP)
     console.log("Gia tri id moi duoc insert la: ", result.insertedId.toHexString());
     res.redirect('/product')
+})
+
+app.get('/delete',async(req,res)=>{
+    const id = req.query.id
+    console.log("id need delete: " + id)
+    const dbo = await getDatabase()
+    dbo.collection("Products").deleteOne({_id:ObjectId(id)})
+    res.redirect('/')
+})
+
+app.get('/edit/:id',async(req,res)=>{
+    const id = req.params.id
+    //lay information old of product before edit
+    const dbo = await getDatabase()
+    const result = await dbo.collection("Products").findOne({_id:ObjectId(id)})
+    //hien thi ra de sua
+    res.render("edit",({product:result}))
 })
 
 const PORT = process.env.PORT || 5000
